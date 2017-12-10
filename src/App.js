110aecc515b5ef1fb5286420140cd897ca7b51fc
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-10 14:32:09 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <form className="registrar">
+            <input type="text" name="name" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit" className="submitButton">Submit</button>
+          </form>
+        </header>
+        <div className="wrapper">
+          <div className="main">
+            <h2>Invitees</h2>
+            <div className="subMain">
+              <label>Hide those who haben't responded</label>
+              <input type="checkbox" className="filterCheck" />
+            </div>
+          </div>
+          <ul className="invitedList">
+            <li>
+              <span>Jerry</span>
+              <label>Confirm
+                <input type="checkbox" className="filterCheck" />
+              </label>
+              <button className="edit">edit</button>
+              <button className="remove">remove</button>
+            </li>
+          </ul>
+          <button name="clear" value="clear" className="clear">Clear</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
